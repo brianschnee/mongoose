3e48c0dbb0ef3493a7f1e660608db90de2b4eb86
brianschnee <brian.a.schnee@gmail.com>
2022-08-25 16:23:53 -0400

    add user, find user

--- script.js
+++ script.js
@@ -6,21 +6,29 @@ mongoose.connect(process.env.DB_STRING, () => {
     console.log('connected to db')
 }, e => console.error(e))
 
-run()
-async function run() {
+add('Brian', 24, 'test@test2.com', ['Bowling'], { street: 'Main Street' })
+find(20, 'Brian')
+
+async function add(name, age, email, hobbies, address) {
     try {
         const user = await User.create({
-            name: 'Brian',
-            age: 24,
-            email: 'test@test.com',
-            hobbies: ['Weight Lifting', 'Bowling'],
-            address: {
-                street: 'Main street'
-            }
+            name: name,
+            age: age,
+            email: email,
+            hobbies: hobbies,
+            address: address
         })
-
         console.log(user)
     } catch (e) {
         console.error(e.message)
+    }
+}
+
+async function find(age, name) {
+    try {
+        const user = await User.where('name').equals(name).where('age').gt(age)
+        console.log({ found: user })
+    } catch (err) {
+        console.error(err)
     }
 }
